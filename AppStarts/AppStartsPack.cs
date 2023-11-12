44f8b28b3b59b0e09e2f18d8333c8eea0498f036
Anton Roshchin <ARochtchin@gmail.com>
2023-11-12 20:18:01 +0300

    Bug fix: window size changing AppStarts plugin

---
+++ AppStarts/AppStartsPack.cs
@@ -0,0 +1,16 @@
+﻿using DesktopPet.Models;
+using System.ComponentModel.Composition;
+
+namespace AppStarts
+{
+    [Export(typeof(IScenarioPack))]
+    public class AppStartsPack : IScenarioPack
+    {
+        public string Name => "Application start";
+
+        public IScenario[] scenarios => new IScenario[]
+        {
+            new StartIt(new AppItem("VisualStudio", "avares://AppStarts/Images/VS.gif", new int2(680,422)))
+        };
+    }
+}
---
+++ AppStarts/StartIt.cs
@@ -0,0 +1,54 @@
+﻿using DesktopPet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppStarts
+{
+    class AppItem
+    {
+        internal AppItem(string name, string gifPath, int2 size)
+        {
+            Name = name;
+            GifPath = gifPath;
+            Size = size;
+        }
+
+        public string Name { get; private set; }
+        public string GifPath { get; private set; }
+        public int2 Size { get; private set; }
+    }
+
+    internal class StartIt : IScenario
+    {
+        AppItem _appItem;
+        public StartIt(AppItem item)
+        {
+            _appItem= item;
+        }
+
+        public string Title => _appItem.Name;
+
+        public string Gif => _appItem.GifPath;
+
+        public int TimerInterval => 5000;
+
+        public InitResult Initialize(int2 windPos, int2 screenSize)
+        {
+            var size  = _appItem.Size;
+            return new InitResult(new int2(screenSize.x/2 - size.x / 2, screenSize.y/2 - size.y / 2), size);
+        }
+
+        public MoveResult OnMove(int2 pos, int2 screen)
+        {
+            return new MoveResult(pos).Finish();
+        }
+
+        public void OnPause(bool pause)
+        {
+            
+        }
+    }
+}
